0f6c76d6369e343afffa193e3bbd55b63175169d
monkfishfillet <monkfishfillet@gmail.com>
2022-04-05 17:14:04 -0400

    Refactored random drink Functionality + Reduced try/except

--- cogs/Bartender.py
+++ cogs/Bartender.py
@@ -14,49 +14,57 @@ class Bartender(commands.Cog, name='Bartender', description=BARTENDER_DESCRIPTIO
     @commands.command(name='drink', description=DRINK_DESCRIPTION, usage=DRINK_USAGE)
     async def drink(self, ctx, *args):
         FILTER_TYPES = ['a','bourbon','brandy','cognac','gin', 'na','rum','scotch','tequila','vodka','whiskey','wine']
-        drink_url = 'https://www.thecocktaildb.com/api/json/v1/1/'
+        DRINK_URL = 'https://www.thecocktaildb.com/api/json/v1/1/'
+ 
+        if not args:
+            drink = await self.get_random_drink(ctx, DRINK_URL)
+            await ctx.send(embed=drink.embed)
+            return
 
+            
+            
+        if len(args) > 1:
+            await ctx.send(f'I think you added too many arguments there, bud.')
+            return
+
+        arg = args[0].lower()
+        if arg not in FILTER_TYPES:
+                message = 'Better check your arguments there, bud.'
+                chance = r.randint(1,100)
+                if chance > 80:
+                    message += ' (Kinda cringe...)'
+                await ctx.send(message)
+                return
+        
+        
+        if arg == 'a':
+            filter = 'filter.php?a=Alcoholic' 
+            resolved_url = self.drink_url_factorty(drink_url, filter)
+        elif arg == 'na':
+            filter = 'filter.php?a=Non_Alcoholic'
+            resolved_url = self.drink_url_factorty(drink_url, filter)
+        else:
+            filter = f'filter.php?i={arg}'
+            resolved_url = self.drink_url_factorty(drink_url, filter)
+        
+        json = req.get(resolved_url).json()
+        drink = Drink(json['drinks'][0])
+        await ctx.send(embed = drink.embed)
+
+
+    async def get_random_drink(self,ctx:commands.Context, DRINK_URL:str):
+        filter_drink_url = DRINK_URL + 'random.php'
         try:
-            if not args:
-                drink_url += 'random.php'
-                json = req.get(drink_url).json()
-                drink = Drink(json['drinks'][0])
-                await ctx.send(embed = drink.embed)
-                return
-            
-
-            if len(args) > 1:
-                await ctx.send(f'I think you added too many arguments there, bud.')
-                return
-
-            arg = args[0].lower()
-            if arg not in FILTER_TYPES:
-                    message = 'Better check your arguments there, bud.'
-                    chance = r.randint(1,100)
-                    if chance > 80:
-                        message += ' (Kinda cringe...)'
-                    await ctx.send(message)
-                    return
-            
-            
-            if arg == 'a':
-                filter = 'filter.php?a=Alcoholic' 
-                resolved_url = self.drink_url_factorty(drink_url, filter)
-            elif arg == 'na':
-                filter = 'filter.php?a=Non_Alcoholic'
-                resolved_url = self.drink_url_factorty(drink_url, filter)
-            else:
-                filter = f'filter.php?i={arg}'
-                resolved_url = self.drink_url_factorty(drink_url, filter)
-            
-            json = req.get(resolved_url).json()
-            drink = Drink(json['drinks'][0])
-            await ctx.send(embed = drink.embed)
-
-        except Exception as ex:
+            res = req.get(filter_drink_url)
+            res.raise_for_status()
+        except req.HTTPError as ex:
             traceback.print_exc()
-            await ctx.send(f'Ayo, your code is wack.\n Error: {ex}')
+            await ctx.send(f'Ayo, something went wrong with the request: \n{ex}')
 
+        
+        json = res.json()
+        drink = Drink(json['drinks'][0])
+        return drink
 
     def drink_url_factorty(self, drink_url, filter):
         filter_url = drink_url + filter
